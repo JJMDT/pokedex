daa46edd29b9c88dff74af38d3be4f52215745fb
juanjo <juanjo.mdt@gmail.com>
2023-05-31 11:14:38 -0300

    se esta realizando modal con inf del pokemon

--- app.js
+++ app.js
@@ -8,9 +8,19 @@ const ver =document.getElementById('verTodo');
 const totalPokemon= document.getElementById('pokemonCount');
 
 let contador = 0;
+const modal = document.createElement('div');
+modal.style.display = 'none';
+modal.style.position = 'fixed';
+modal.style.top = '0';
+modal.style.left = 'auto';
+modal.style.width = '100%';
+modal.style.height = 'auto';
+modal.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+modal.classList.add('modal');
+document.body.append(modal);
 
 
-for (let i=1; i<= ver; i++){
+for (let i=1; i<= 0; i++){
     fetch(URL +i)
         .then((response) => response.json())
         .then(data => mostrarPokemon(data))
@@ -26,6 +36,7 @@ function mostrarPokemon(pokemon) {
               
               tipos = tipos.join("");
 
+    
     const div= document.createElement('div');
     div.classList.add('cardPokemon');
     div.innerHTML =`
@@ -44,7 +55,62 @@ function mostrarPokemon(pokemon) {
             </div>
      </div>
     `;
+ 
+    div.addEventListener('click',()=> {
+        console.log(`hiciste click en ${pokemon.name}`)
+        const modal = document.createElement('div');
+        modal.classList.add('modal', 'fade');
+        modal.tabIndex = -1;
+        modal.innerHTML = `
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title">${pokemon.name}</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+              </div>
+              <div class="modal-body">
+                <p>EXP: ${pokemon.base_experience}</p>
+              </div>
+            </div>
+          </div>
+        `;
+        document.body.append(modal);
+        const bootstrapModal = new bootstrap.Modal(modal);
+        bootstrapModal.show();
+    
+      
+    //     modal.style.display = 'block';
+    // modal.innerHTML = `
+    //   <div  style="background-color: white; padding: 20px;">
 
+    //      <div class="imgPokemon">
+    //             <img src="${pokemon.sprites.other['official-artwork'].front_default}" alt="">
+    //         </div>
+    //         <div class="nombrePokemon">
+    //             <p id="idPokemon"> #${pokemon.id} </p>
+    //             <h2 id="nombrePokemon"> ${pokemon.name}</h2> 
+    //         </div>
+    //         <div class="tipoPokemon">
+    //             ${tipos}
+    //         </div>
+    //         <div class="stats">
+    //         <p class="stat">EXP ${pokemon.base_experience}</p>
+          
+    //         <p class="stat">Height ${pokemon.height}</p>
+          
+    //         <p class="stat">Weight ${pokemon.weight}</p>
+    //         <p class="stat">Weight ${pokemon.weight}</p>
+    //         <p class="stat">Weight ${pokemon.weight}</p>
+    //         </div>
+    //         <button id="closeModal">Cerrar</button>
+    //  </div>
+    // `;
+    const closeModalButton = document.querySelector('#closeModal');
+    closeModalButton.addEventListener('click', () => {
+      modal.style.display = 'none';})
+
+    });
+    
     listaPokemon.append(div);
     contador ++;
     mostrarPokemonConsola(pokemon)
@@ -71,5 +137,4 @@ botonHeader.forEach(boton => boton.addEventListener('click', (event)=>{
 function mostrarPokemonConsola(pokemon) {
     totalPokemon.innerHTML=`Nro de resultado ${contador} pokemon`
     
-
 }
